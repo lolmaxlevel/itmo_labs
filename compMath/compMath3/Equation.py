76854e06e09117e0e345f2434d7c9086c2796922
Ternovkiy Ilya <lolmaxlevel@gmail.com>
2023-04-18 18:38:55 +0300

    completed lab3 compMath

---
+++ compMath/compMath3/Equation.py
@@ -0,0 +1,5 @@
+class Equation:
+    def __init__(self, f, discontinuity_points, string_form):
+        self.f = f
+        self.discontinuity_points = discontinuity_points
+        self.string_form = string_form
--- compMath/compMath3/main.py
+++ compMath/compMath3/main.py
@@ -1,4 +1,17 @@
 import math
+import scipy.integrate as integrate
+
+from Equation import Equation
+
+
+def remove_discontinuity(f):
+    def g(x):
+        if f(x):
+            return f(x)
+        else:
+            return 0.5 * (f(x + 1e-6) + f(x - 1e-6))
+
+    return g
 
 
 def integrate_middle_rect(f, a, b, n):
@@ -13,7 +26,6 @@ def integrate_middle_rect(f, a, b, n):
     for i in range(n):
         area += f(x) * dx  # вычисляем площадь текущего прямоугольника
         x += dx  # перемещаемся к центру следующего прямоугольника
-
     return area
 
 
@@ -45,9 +57,53 @@ def integrate_right_rect(f, a, b, n):
     for i in range(n):
         area += f(x) * dx  # вычисляем площадь текущего прямоугольника
         x += dx  # перемещаемся к правому краю следующего прямоугольника
-
     return area
 
 
-result = integrate_middle_rect(lambda x: math.sin(x), 0, 1, 100)
-print(result)
+def equation_1(x):
+    if x > 0:
+        return 1
+    elif x < 0:
+        return 0
+
+
+equations = [Equation(lambda x: x ** 2, [], "x^2"),
+             Equation(lambda x: math.sin(x), [], "sin(x)"),
+             Equation(lambda x: math.cos(x), [], "cos(x)"),
+             Equation(equation_1, [0], "x=1, x>0\nx=0, x<0"),
+             ]
+
+try:
+    equation, a, b, n = equations[int(input("Выберите какое уравнение решать: \n" + "\n".join(
+            [str(i + 1) + "." + equations[i].string_form for i in range(len(equations))]) + "\n")) - 1], \
+            int(input("Введите левую границу: ")), \
+            int(input("Введите правую границу: ")), int(input("Введите количество прямоугольников: "))
+    if n < 1:
+        raise ValueError("Количество прямоугольников должно быть больше 0")
+    if a > b:
+        raise ValueError("Левая граница должна быть меньше правой")
+except (IndexError, ValueError) as e:
+    if e == IndexError:
+        print("Такого уравнения нет, перезапустите программу и введите номер уравнения из списка")
+    if e == ValueError:
+        print("Неверный формат ввода, перезапустите программу и введите число")
+    exit(1)
+
+
+if equation.discontinuity_points and a < equation.discontinuity_points[0] < b:
+    left_result = integrate_left_rect(remove_discontinuity(equation.f), a, b, n)
+    right_result = integrate_right_rect(remove_discontinuity(equation.f), a, b, n)
+    middle_result = integrate_middle_rect(remove_discontinuity(equation.f), a, b, n)
+else:
+    left_result = integrate_left_rect(equation.f, a, b, n)
+    right_result = integrate_right_rect(equation.f, a, b, n)
+    middle_result = integrate_middle_rect(equation.f, a, b, n)
+
+
+correct = integrate.quad(remove_discontinuity(equation.f), a, b)[0]
+
+
+print(f"Результат методом левых прямоугольников: {left_result} ошибка: {abs(correct - left_result)}")
+print(f"Результат методом правых прямоугольников: {right_result} ошибка: {abs(correct - right_result)}")
+print(f"Результат методом центральных прямоугольников: {middle_result} ошибка: {abs(correct - middle_result)}")
+print(f"Правильный ответ: {correct}")
