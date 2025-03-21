05ecb5c37c90110465fa4ce99863eb2b50b97cc6
Ilya Ternovskiy <lolmaxlevel@gmail.com>
2025-03-21 13:23:29 +0300

    hello its me.....x2

---
+++ TPO/tpo_lab2/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "org.lolmaxlevel"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.platform:junit-platform-launcher:1.10.2")
+    testImplementation("org.junit.platform:junit-platform-engine:1.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testImplementation("org.junit.platform:junit-platform-suite-api:1.10.2")
+    testImplementation("org.junit.platform:junit-platform-suite-engine:1.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.1")
+    testImplementation("org.mockito:mockito-core:5.8.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-console-standalone:1.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(22)
+}
---
+++ TPO/tpo_lab2/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package org.lolmaxlevel
+
+import org.lolmaxlevel.trigonometric.*
+
+fun main() {
+    println(TrigonometricFunction().calculate(1.0))
+}
